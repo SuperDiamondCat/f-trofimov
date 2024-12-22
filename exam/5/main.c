bc0ea0fc0ecafc8f1f91f7db8978c10a0a0a6086
Gleb Shadrin <shadringleb@hotmail.com>
2024-12-22 16:51:27 +0500

    пятая задача, прости господи

--- exam/5/main.c
+++ exam/5/main.c
@@ -15,26 +15,88 @@ bool is_prime(int num) {
 }
 
 // Функция для нахождения всех простых чисел до N
-int* find_primes(int N) {
+long *find_primes(int N, int *count) {
 
-    int *primes = (int *)malloc(sizeof(int *));
+    long *primes = (long *)malloc(sizeof(long) * N);
     if(primes == NULL) {
         printf("Ошибка при выделении памяти");
         exit(1);
     }
+
     int j = 0; // Второй счётчик
     for (int i = 2; i < N; i++) {
         if (is_prime(i)) {
-            primes = realloc(primes, sizeof(primes) + sizeof(int *));
             primes[j] = i;
-            j++; 
+            j++;
         }
     }
+
+    // Уменьшаем размер массива до реального количества найденных чисел
+    primes = (long *)realloc(primes, j * sizeof(long));
+    if (primes == NULL && j > 0) {
+        printf("Ошибка при повторном выделении памяти\n");
+        exit(1);
+    }    
+
+    *count = j;
     return primes;
 }
 
 void test() {
-    int primes[] = find_primes(10);
+    {
+        int N = 15;
+        int count = 0;
+        long *primes = find_primes(N, &count);
+
+        // for(int i = 0; i < count; i++) {
+        //     printf("%ld ", primes[i]);
+        // }
+        // printf("\n");
+
+        if(primes[4] != 11) {
+            free(primes);
+            printf("Ошибка 1");
+            exit(1);
+        }
+
+        free(primes);
+    }
+    {
+        int N = 5;
+        int count = 0;
+        long *primes = find_primes(N, &count);
+
+        // for(int i = 0; i < count; i++) {
+        //     printf("%ld ", primes[i]);
+        // }
+        // printf("\n");
+
+        if(primes[1] != 3) {
+            free(primes);
+            printf("Ошибка 2");
+            exit(2);
+        }
+
+        free(primes);
+    }
+    {
+        int N = 0;
+        int count = 0;
+        long *primes = find_primes(N, &count);
+
+        // for(int i = 0; i < count; i++) {
+        //     printf("%ld ", primes[i]);
+        // }
+        // printf("\n");
+
+        if(count != 0) {
+            free(primes);
+            printf("Ошибка 3");
+            exit(3);
+        }
+
+        free(primes);
+    }
 }
 
 int main() {
